4960b9c7682ed8a62cf0cdf67d54fa45426f7eec
Andrei Filip <andrei.stefan.filip@gmail.com>
2024-12-28 17:15:01 +0200

    Laborator9

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -3,10 +3,10 @@ namespace FilipAndreiLab7;
 
 public partial class ListPage : ContentPage
 {
-	public ListPage()
-	{
-		InitializeComponent();
-	}
+    public ListPage()
+    {
+        InitializeComponent();
+    }
 
     async void OnSaveButtonClicked(object sender, EventArgs e)
     {
@@ -22,4 +22,45 @@ public partial class ListPage : ContentPage
         await Navigation.PopAsync();
     }
 
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+        this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+
+    async void OnDeleteItemButtonClicked(object sender, EventArgs e)
+    {
+        var selectedProduct = listView.SelectedItem as Product;
+        var shopList = (ShopList)BindingContext;
+
+        if (selectedProduct != null)
+        {
+            var listProduct = await App.Database.GetListProductAsync(shopList.ID, selectedProduct.ID);
+            if (listProduct != null)
+            {
+                await App.Database.DeleteListProductAsync(listProduct);
+                listView.ItemsSource = await App.Database.GetListProductsAsync(shopList.ID);
+            }
+            else
+            {
+                await DisplayAlert("Error", "Product not found in the list", "OK");
+            }
+        }
+        else
+        {
+            await DisplayAlert("Error", "No product selected", "OK");
+        }
+
+    }
 }
